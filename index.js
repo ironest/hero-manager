e9d9c17bcad691311a9fd9e0e8b8395de9e43ead
ironest <ironest@gmail.com>
2020-01-05 23:35:49 +1100

    Established connection to the DB

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+/* Makes all variables from our .env file available in our process */
+require('dotenv').config();
+
+/* Establishes the connection to the database */
+require("./database/connect");
+
+/* Loads all the core functionalities */
 const app = require("./app");
 
-/* Make all variables from our .env file available in our process */
-require('dotenv').config();
-const port = process.env.PORT;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+/* Starts the express app */
+app.listen(process.env.PORT, () => console.log(`Listening on port ${port}`));
