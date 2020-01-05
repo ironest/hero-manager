9ea087d3033d89dc6e70d9f2b249349eddf4de75
ironest <ironest@gmail.com>
2020-01-05 23:24:36 +1100

    Included ejs into the app

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const ejsLayouts = require("express-ejs-layouts");
 
 /* This is the former BODY-PARSER which provides req.body */
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+/* Setting up EJS as view engine */
+app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/main");
+app.use(ejsLayouts);
+app.use(express.static("public"));
 
 /* Define a route */
 app.use(require("./routes"));
